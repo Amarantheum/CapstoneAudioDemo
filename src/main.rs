be658e9beea00c714ea1d94359d9564bb6b88750
Amarantheum <kougharagua@gmail.com>
2023-03-31 01:25:48 -0700

    add basic audio player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+use eframe::{egui, CreationContext};
+use egui::Stroke;
+
+struct AudioPlayer {
+    playing: bool,
+    progress: f32,
+}
+
+impl Default for AudioPlayer {
+    fn default() -> Self {
+        Self {
+            playing: false,
+            progress: 0.0,
+        }
+    }
+}
+
+impl AudioPlayer {
+    fn new(_cc: &CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for AudioPlayer {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        let Self { playing, progress } = self;
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if ui.button(if *playing { "⏸" } else { "▶" }).clicked() {
+                *playing = !*playing;
+            }
+
+            let (response, painter) = ui.allocate_painter(ui.available_size_before_wrap(), egui::Sense::click());
+            painter.rect(
+                response.rect,
+                2.0,
+                ui.visuals().extreme_bg_color,
+                Stroke::default(),
+            );
+            painter.rect(
+                egui::Rect::from_min_size(
+                    response.rect.min,
+                    egui::vec2(response.rect.width() * *progress, response.rect.height()),
+                ),
+                2.0,
+                ui.visuals().selection.bg_fill,
+                Stroke::default(),
+            );
+
+            if response.clicked() {
+                let click_pos = response.interact_pointer_pos().unwrap();
+                let new_progress = (click_pos.x - response.rect.min.x) / response.rect.width();
+                *progress = new_progress;
+                println!("Clicked progress bar at position: {}", new_progress);
+            }
+        });
+
+        if *playing {
+            if *progress > 1.0 {
+                *progress = 0.0;
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("AudioPlayer", native_options, Box::new(|cc| Box::new(AudioPlayer::new(cc)))).expect("failed to start");
 }
---
+++ src/stream.rs
@@ -0,0 +1,54 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Sample, FromSample};
+use std::error::Error;
+use lazy_static::lazy_static;
+use parking_lot::Mutex;
+
+pub static mut LOC: usize = 0;
+lazy_static! {
+    pub static ref AUDIO: Mutex<[Vec<f32>; 2]> = Mutex::new([vec![], vec![]]);
+    pub static ref P: Mutex<bool> = Mutex::new(false);
+}
+pub fn prepare_cpal_stream() -> Result<cpal::Stream, Box<dyn Error>> {
+    let host = cpal::default_host();
+    let device = host.default_output_device().ok_or("No output device available")?;
+
+    let mut supported_configs_range = device.supported_output_configs()
+        .map_err(|e| format!("Error while querying configs: {}", e))?;
+    let supported_config = supported_configs_range.next()
+        .ok_or("No supported config found")?
+        .with_max_sample_rate();
+
+    let stream = device.build_output_stream(
+        &supported_config.into(),
+        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+            write_silence(data)
+        },
+        move |err| {
+            // react to errors here.
+        },
+        None // None=blocking, Some(Duration)=timeout
+    ).map_err(|e| format!("Error while building output stream: {}", e))?;
+    stream.play().map_err(|e| format!("Failed to play stream: {}", e))?;
+
+    Ok(stream)
+}
+
+fn write_silence<T: Sample + FromSample<f32>>(data: &mut [T]) {
+    let audio = AUDIO.lock();
+    let mut cur = 0;
+    if *P.lock() {
+        for sample in data.iter_mut() {
+            *sample = unsafe { audio[cur][LOC].to_sample() };
+            cur = (cur + 1) % 2;
+            if cur == 0 {
+                unsafe { LOC += 1; }
+            }
+        }
+    } else {
+        for sample in data.iter_mut() {
+            *sample = Sample::EQUILIBRIUM;
+        }
+    }
+    
+}
